2ad2fa0fb59a562b047cf3d7b78e809f05de8ae1
迟慧璇 <chihuixuan@chihuixuandeMacBook-Pro.local>
2021-02-20 17:30:25 +0800

    add GCN_res

--- full-ogbn.py
+++ full-ogbn.py
@@ -10,6 +10,8 @@ import torch_geometric.transforms as T
 
 from ogb.nodeproppred import PygNodePropPredDataset, Evaluator
 
+from logger import Logger
+
 """
 批处理：full-batch
 图数据表示方法：SpMM
@@ -190,6 +192,8 @@ class JKNet(nn.Module):
             conv.reset_parameters()
         for bn in self.bns:
             bn.reset_parameters()
+        self.jk.reset_parameters()
+        self.fc.reset_parameters()
 
     def forward(self, data):
         x, adj_t = data.x, data.adj_t
@@ -209,11 +213,80 @@ class JKNet(nn.Module):
         return h
 
 
+# ResGCN
+class GCN_res(nn.Module):
+    def __init__(self, dataset, hidden=256, num_layers=6):
+        super(GCN_res, self).__init__()
+
+        self.num_layers = num_layers
+        self.convs = nn.ModuleList()
+        self.bns = nn.ModuleList()
+
+        self.input_fc = nn.Linear(dataset.num_node_features, hidden)
+
+        # self.convs.append(GCNConv(dataset.num_node_features, hidden))
+        # self.bns.append(nn.BatchNorm1d(hidden))
+
+        for i in range(self.num_layers):
+            self.convs.append(GCNConv(hidden, hidden))
+            self.bns.append(nn.BatchNorm1d(hidden))
+
+        self.out_fc = nn.Linear(hidden, dataset.num_classes)
+        self.weights = torch.nn.Parameter(torch.randn((len(self.convs))))
+
+        # self.jk = JumpingKnowledge(mode=mode)
+        # if mode == 'max':
+        #     self.fc = nn.Linear(hidden, dataset.num_classes)
+        # elif mode == 'cat':
+        #     self.fc = nn.Linear(num_layers * hidden, dataset.num_classes)
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for bn in self.bns:
+            bn.reset_parameters()
+        # self.jk.reset_parameters()
+        # self.fc.reset_parameters()
+        self.input_fc.reset_parameters()
+        self.out_fc.reset_parameters()
+        torch.nn.init.normal_(self.weights)
+
+    def forward(self, data):
+        x, adj_t = data.x, data.adj_t
+
+        x = self.input_fc(x)
+        x_input = x  # .copy()
+
+        layer_out = []  # 保存每一层的结果
+        for i in range(self.num_layers):
+            x = self.convs[i](x, adj_t)
+            x = self.bns[i](x)  # 小数据集不norm反而效果更好
+            x = F.relu(x, inplace=True)
+            x = F.dropout(x, p=0.5, training=self.training)
+            x = x + 0.2 * x_input
+            layer_out.append(x)
+
+        # h = self.jk(layer_out)  # JK层
+        # h = self.fc(h)
+        # h = F.log_softmax(h, dim=1)
+
+        weight = F.softmax(self.weights, dim=0)
+        for i in range(len(layer_out)):
+            layer_out[i] = layer_out[i] * weight[i]
+
+        x = sum(layer_out)
+        x = self.out_fc(x)
+        x = F.log_softmax(x, dim=1)
+
+        return x
+
+
 # 实例化模型
-model = GCNNet(dataset=dataset, hidden=256, num_layers=3)
+# model = GCNNet(dataset=dataset, hidden=256, num_layers=3)
 # model = GATNet(dataset=dataset, hidden=128, num_layers=2)
 # model = SAGENet(dataset=dataset, hidden=256, num_layers=3)
 # model = JKNet(dataset=dataset, mode='max', hidden=128, num_layers=5)
+model = GCN_res(dataset=dataset, hidden=128, num_layers=6)
 print(model)
 
 # 转换为cpu或cuda格式
@@ -269,9 +342,26 @@ def test():
 
 # 程序入口
 if __name__ == '__main__':
-    for epoch in range(200):
-        loss = train()
-        print('Epoch {:03d} train_loss: {:.4f}'.format(epoch, loss))
+    runs = 2
+    logger = Logger(runs)
 
-        train_acc, val_acc, test_acc = test()
-        print(f'Train: {train_acc:.4f}, Val: {val_acc:.4f}, 'f'Test: {test_acc:.4f}')
+    for run in range(runs):
+        model.reset_parameters()
+
+        for epoch in range(500):
+            loss = train()
+            # print('Epoch {:03d} train_loss: {:.4f}'.format(epoch, loss))
+
+            result = test()
+            train_acc, valid_acc, test_acc = result
+            # print(f'Train: {train_acc:.4f}, Val: {valid_acc:.4f}, 'f'Test: {test_acc:.4f}')
+            print(f'Run: {run + 1:02d}, '
+                  f'Epoch: {epoch:02d}, '
+                  f'Loss: {loss:.4f}, '
+                  f'Train: {100 * train_acc:.2f}%, '
+                  f'Valid: {100 * valid_acc:.2f}% '
+                  f'Test: {100 * test_acc:.2f}%')
+
+            logger.add_result(run, result)
+
+    logger.print_statistics()
